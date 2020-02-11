ef75f5e5d31026eee41a9670ce663587532aabac
Maia CHARDIN <mchardin@e1r11p1.42.fr>
2020-02-11 20:49:32 +0100

    nginx ok, mysql en cours

--- setup.sh
+++ setup.sh
@@ -1,7 +1,18 @@
+rm -rf srcs/SSL
+echo "--------------------------------------------------------"
+echo "----------------- STARTING MINIKUBE --------------------"
+echo "--------------------------------------------------------"
+
 minikube start --cpus=4 --memory 4000 --disk-size 11000 --vm-driver virtualbox --extra-config=apiserver.service-node-port-range=1-35000
-# minikube dashboard
-kubectl apply -f srcs/nginx-deployment.yaml
-kubectl apply -f srcs/nginx-service.yaml
-kubectl apply -f srcs/ingress.yaml
-kubectl get ingress test-ingress
+minikube addons enable ingress
 eval $(minikube docker-env)
+
+echo "--------------------------------------------------------"
+echo "---------------- CREATING CONTAINERS -------------------"
+echo "--------------------------------------------------------"
+
+kubectl apply -f srcs/Nginx
+kubectl apply -f srcs/Ingress-controller
+kubectl apply -f srcs/MySQL
+
+
