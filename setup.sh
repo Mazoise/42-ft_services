17db8c07a4e5f2fbc9e310195e1ba555679d3d4b
mchardin <mchardin@student.42.fr>
2020-09-17 18:34:27 +0200

    New start

--- setup.sh
+++ setup.sh
@@ -3,16 +3,20 @@ echo "--------------------------------------------------------"
 echo "----------------- STARTING MINIKUBE --------------------"
 echo "--------------------------------------------------------"
 
-minikube start --cpus=4 --memory 4000 --disk-size 11000 --vm-driver virtualbox --extra-config=apiserver.service-node-port-range=1-35000
-minikube addons enable ingress
-eval $(minikube docker-env)
+# minikube start --vm-driver docker #
+# eval $(minikube docker-env) #utiliser le docker de minikube
+#sudo usermod -aG docker user42 ## CHECK IF DOCKER WORKS BEFORE
+
+echo "--------------------------------------------------------"
+echo "-------------- BUILDING DOCKER IMAGES ------------------"
+echo "--------------------------------------------------------"
+
+docker build -t nginx srcs/nginx
+docker run -itd -p 8080:80 --name nginx nginx
 
 echo "--------------------------------------------------------"
 echo "---------------- CREATING CONTAINERS -------------------"
 echo "--------------------------------------------------------"
 
-kubectl apply -f srcs/Nginx
-kubectl apply -f srcs/Ingress-controller
-kubectl apply -f srcs/MySQL
-
-
+# kubectl apply -f srcs/Nginx
+# kubectl apply -f srcs/MySQL
